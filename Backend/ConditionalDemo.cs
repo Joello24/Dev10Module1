3c99e9a239ceae665f985e7a06840bd9a355b90a
Robert Lillyblad <robert.lillyblad@gmail.com>
2022-03-10 10:13:16 -0600

    IsWithinRange added to ConditionalDemo

---
+++ Backend/ConditionalDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Backend
+{
+    static internal class ConditionalDemo
+    {
+        /// <summary>
+        /// /// <summary>
+        /// Given a non-negative number, return true if the number is within range of a multiple of the target.
+        // Given 12, 10, and  2 return true
+        // Given 23, 10, and  3 return true
+        // Given 17, 5, and  1 return false)]
+        // Given 17, 0, and  1 return false)]
+        /// </summary>
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public static bool isWithinRange(int num, int target, int range) {
+            if (num < 0)
+            {
+                return false;
+            }
+            else if ((num % target) <= range)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
--- Backend/Controller.cs
+++ Backend/Controller.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HelloWorld.Backend;
+using static HelloWorld.Backend.ConditionalDemo;
+
 
 namespace HelloWorld
 {
@@ -16,9 +18,10 @@ namespace HelloWorld
             //StringConcat tester2 = new StringConcat();
             //tester2.Run();
 
-            Tip tester3 = new Tip();
-            tester3.Run();
+            //Tip tester3 = new Tip();
+            //tester3.Run();
 
+            Console.WriteLine($"Output: {ConditionalDemo.isWithinRange(12, 10, 2).ToString()} Hi");
         }
     }
 }
